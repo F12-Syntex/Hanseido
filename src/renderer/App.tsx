ce9ead709562e8884113707b4bc3f7d57502472a
Syntex <30322632+F12-Syntex@users.noreply.github.com>
2023-09-01 08:22:45 +0100

    v1.0.3
    Fixed prev push

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -5,12 +5,10 @@ import { Route, HashRouter as Router, Routes } from 'react-router-dom';
 import './App.css';
 import Home from '@Components/home/Home';
 import ForgotPassword from '@Components/forgot-password/ForgotPassword';
-import NavBar from '@Components/nav-bar/NavBar';
 
 function App() {
   return (
     <Router>
-      <NavBar />
       <div className="content-pane">
         <Routes>
           <Route path="/" element={<Home />} />
